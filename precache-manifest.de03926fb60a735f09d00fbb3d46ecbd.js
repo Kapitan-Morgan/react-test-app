0eb97d83b4acce8afc9573225c22367659866bbe
Яромир Морган <yaromirmorgan@gmail.com>
2020-02-24 15:24:36 +0300

    Updates

---
+++ precache-manifest.de03926fb60a735f09d00fbb3d46ecbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b72776928b34176efabc916c2597e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcfb2cbe9acfefc62eae",
+    "url": "/static/css/main.bc0a968a.chunk.css"
+  },
+  {
+    "revision": "2edc3e6175de497b7124",
+    "url": "/static/js/2.22e980c7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.22e980c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfb2cbe9acfefc62eae",
+    "url": "/static/js/main.ab2fb45a.chunk.js"
+  },
+  {
+    "revision": "bcce9b594c29f459179d",
+    "url": "/static/js/runtime-main.d469b0c1.js"
+  }
+]);
